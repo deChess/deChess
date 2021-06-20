951ba95429240307d296de6bee0832677c5d4908
Ali Abdoli <aliabdoli1970@gmail.com>
2021-06-20 01:04:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import Chess from "chess.js"
+import Chessground from "react-chessground"
+import "react-chessground/dist/styles/chessground.css"
+import { Col, Modal, Row } from "antd"
+import queen from "./images/wQ.svg"
+import rook from "./images/wR.svg"
+import bishop from "./images/wB.svg"
+import knight from "./images/wN.svg"
+import 'antd/dist/antd.css';
+
 
 function App() {
+  const [chess] = useState(new Chess())
+  const [pendingMove, setPendingMove] = useState()
+  const [selectVisible, setSelectVisible] = useState(false)
+  const [fen, setFen] = useState("")
+  const [lastMove, setLastMove] = useState()
+  const [isChecked, setChecked] = useState(false)
+
+  const onMove = (from, to) => {
+    const moves = chess.moves({ verbose: true })
+    for (let i = 0, len = moves.length; i < len; i++) { /* eslint-disable-line */
+      if (moves[i].flags.indexOf("p") !== -1 && moves[i].from === from) {
+        setPendingMove([from, to])
+        setSelectVisible(true)
+        console.log(selectVisible);
+        return
+      }
+    }
+    if (chess.move({ from, to, promotion: "q" })) {
+      setFen(chess.fen())
+      setLastMove([from, to])
+      // setTimeout(randomMove, 500)
+      setChecked(chess.in_check());
+    }
+  }
+
+  const randomMove = () => {
+    const moves = chess.moves({ verbose: true })
+    const move = moves[Math.floor(Math.random() * moves.length)]
+    if (moves.length > 0) {
+      chess.move(move.san)
+      setFen(chess.fen())
+      setLastMove([move.from, move.to])
+      setChecked(chess.in_check());
+    }
+  }
+
+  const promotion = e => {
+    const from = pendingMove[0]
+    const to = pendingMove[1]
+    console.log("test");
+
+    chess.move({ from, to, promotion: e })
+    setFen(chess.fen())
+    setLastMove([from, to])
+    setSelectVisible(false)
+    setChecked(chess.in_check());
+    // setTimeout(randomMove, 500)
+  }
+
+  const turnColor = () => {
+    return chess.turn() === "w" ? "white" : "black"
+  }
+
+  const calcMovable = () => {
+    const dests = new Map()
+    chess.SQUARES.forEach(s => {
+      const ms = chess.moves({ square: s, verbose: true })
+      if (ms.length) dests.set(s, ms.map(m => m.to))
+    })
+    return {
+      free: false,
+      dests,
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#2b313c", height: "100vh" }}>
+      <Row>
+        <Col span={6} />
+        <Col span={12} style={{ top: "10%" }}>
+          <Chessground
+            width="38vw"
+            height="38vw"
+            turnColor={turnColor()}
+            movable={calcMovable()}
+            lastMove={lastMove}
+            fen={fen}
+            onMove={onMove}
+            highlight={{
+              check: true,
+            }}
+            check={isChecked}
+            style={{ margin: "auto" }}
+          />
+        </Col>
+        <Col span={6} />
+      </Row>
+      <Modal visible={selectVisible} footer={null} closable={false} centered>
+        <div style={{ textAlign: "center", cursor: "pointer" }}>
+          <span role="presentation" onClick={() => promotion("q")}>
+            <img src={queen} alt="" style={{ width: 50 }} />
+          </span>
+          <span role="presentation" onClick={() => promotion("r")}>
+            <img src={rook} alt="" style={{ width: 50 }} />
+          </span>
+          <span role="presentation" onClick={() => promotion("b")}>
+            <img src={bishop} alt="" style={{ width: 50 }} />
+          </span>
+          <span role="presentation" onClick={() => promotion("n")}>
+            <img src={knight} alt="" style={{ width: 50 }} />
+          </span>
+        </div>
+      </Modal>
     </div>
-  );
+  )
 }
 
 export default App;
