1844c61223f86f438d19d92bd98e0c85890f97e5
Hunter Chen <chenhunter0707@gmail.com>
2021-07-02 01:05:08 -0600

    made buildAHorse function not garbage

---
+++ src/getNFTs.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable indent */
+const covalentAPIKey = 'ckey_6c3f3233e25f4ad1bfe6cfc2403';
+
+const ChainIDs = {
+    ethereum: 1,
+    polygon: 137,
+    binance: 56,
+    avalanche: 43114,
+    fantomOpera: 250,
+};
+
+const supportedNetworks = ['ethereum', 'polygon'];
+
+function getNFTsFromChain(network, address) {
+  return null;
+}
+
+function getNFTs(address) {
+  for (let i = 0; i < supportedNetworks.length; i++) {
+
+  }
+}
